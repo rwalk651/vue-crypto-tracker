79e689e10879b5eee7801f339afb20d6511e5766
Ryan <li5823od@go.minnstate.edu>
2022-05-24 02:54:52 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// https://www.apollographql.com/docs/react/caching/overview/
+// Apollo stores results of GraphQL queries in local, normalized, in-memory-cache.
+// Allows Apollo to respond immediately to queries for already-cached data without network request.
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    cache,
+    uri: '<https://rickandmortyapi.com/graphql>',
+})
+
+const app = createApp({
+    setup () {
+        provide(DefaultApolloClient, apolloClient)
+    },
+
+    render: () => h(App)
+})
+
+app.mount('#app');
---
+++ src/resolvers.js
@@ -0,0 +1,28 @@
+// nomics javascript client will be used to fetch data - https://www.npmjs.com/package/nomics
+import Nomics from 'nomics'
+
+const nomics = Nomics({
+    // environment variable from vue.config.js passed to nomics client
+    apiKey: process.env.API_KEY
+})
+
+export const resolvers = {
+    Query: {
+        // initial coin data fetch
+        getCoins: async (_, args) => {
+            const coins = await nomics.currenciesTicker()
+            // return top 30 coins
+            return coins.slice(0, 30)
+        },
+        // detailed data fetch for an interval (1d, 7d, 30d, 365d, ytd)
+        getCoinDetails: async (_, args) => {
+            const { coinid, interval } = args
+            const fetchCoinDetails = await nomics.currenciesTicker({
+                ids: [`${coinid}`.toUpperCase()],
+                interval: [`${interval}`]
+            })
+
+            return fetchCoinDetails[0][`${interval}`]
+        }
+    }
+}
